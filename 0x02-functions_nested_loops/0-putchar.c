ce9b50c08fad40c9190b06928c35ac38c5762a89
BonganiChauke <bonganichauke11@outlook.com>
2023-08-15 14:23:05 -0700

    created a script called 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program prints putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
